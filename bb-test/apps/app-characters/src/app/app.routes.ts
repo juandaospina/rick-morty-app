51c6eb07ef888b5b85b022ab1d9689ac28b68b6c
Michael Camacho <michael.camacho@triidy.com>
2023-02-22 19:53:27 -0500

    feat(app-characters): add maqueta basic

--- bb-test/apps/app-characters/src/app/app.routes.ts
+++ bb-test/apps/app-characters/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: InicioComponent,
+  },
+];
