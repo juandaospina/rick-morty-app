68b6a6ca260085a40b33ee3380f64365f21b5cd1
Juan Ospina <juan.ospinal@outlook.com>
2023-04-20 21:57:19 -0500

    fix: get called multiple (ngx infinite scroll)

--- bb-test/apps/app-characters/src/app/components/characters-list/characters-list.component.ts
+++ bb-test/apps/app-characters/src/app/components/characters-list/characters-list.component.ts
@@ -1,29 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { DataService } from '../../services/data-service';
 import { Info, Result } from '../../types';
-import { enviroments } from '../../enviroments';
 
 @Component({
-  // standalone: true,
   selector: 'app-characters',
   templateUrl: './characters-list.component.html',
   styleUrls: ['./characters-list.component.scss'],
 })
 export class CharactersListComponent {
+  constructor(
+    private dataService: DataService
+  ) {}
   @Input() apiResult!: Info;
   @Input() characters!: Result[];
   @Input() public name: string = 'Juan Ospina';
   public page: number = 0;
-  constructor(private dataService: DataService) {}
-
-  // ngOnInit(): void {
-  //   const url = `${enviroments.apiUrl}/?page=1`;
-  //   this.dataService.getAllCharacters(url).subscribe((data) => {
-  //     this.apiResult = data.info;
-  //     this.characters = data.results;
-  //   });
-  // }
+  public blockInfiniteScroll: boolean = false;
 
   onHandlerScroll() {
     if (this.apiResult.next) {
@@ -34,6 +27,10 @@ export class CharactersListComponent {
           this.apiResult = data.info;
           this.characters = [...this.characters, ...data.results];
         });
+      this.blockInfiniteScroll = true;
+      setTimeout(() => {
+        this.blockInfiniteScroll = !this.blockInfiniteScroll;
+      }, 2000)
     }
   }
 }
